d319f4ee692d93b34379702add301d46b1fbafb9
DocGrishka <docgrish@gmail.com>
2020-04-19 18:25:38 +0300

    Update user_alg.py

--- algr/user_alg.py
+++ algr/user_alg.py
@@ -3,7 +3,7 @@ from data.users import User
 import requests
 from data.news import SEPARATOR
 
-address = 'https://blooming-eyrie-77799.herokuapp.com'
+address = '127.0.0.1'
 
 
 # address = 'http://127.0.0.1:5000'
