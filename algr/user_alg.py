db9022954f0ff989dc9e72e209e3609ab8994758
DocGrishka <docgrish@gmail.com>
2020-04-19 18:34:47 +0300

    Update user_alg.py

--- algr/user_alg.py
+++ algr/user_alg.py
@@ -3,7 +3,7 @@ from data.users import User
 import requests
 from data.news import SEPARATOR
 
-address = 'http://127.0.0.1:80'
+address = 'http://localhost'
 
 
 # address = 'http://127.0.0.1:5000'
